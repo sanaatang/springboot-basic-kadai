295957ad7ed320c3c8f61bc3750282b0bd122c94
sanaatang <sanaatang@gmail.com>
2025-03-08 01:39:42 +0900

    Initial commit

---
+++ src/main/java/com/example/springkadaitodo/controller/ToDoController.java
@@ -0,0 +1,30 @@
+package com.example.springkadaitodo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.springkadaitodo.entity.ToDo;
+import com.example.springkadaitodo.service.ToDoService;
+
+
+@Controller
+public class ToDoController {
+	private final ToDoService toDoService;
+	
+	public ToDoController(ToDoService toDoService) {
+		this.toDoService = toDoService;
+	}
+	
+	@GetMapping("/todo")
+	public String toDoList(Model model) {
+		List<ToDo> todos = toDoService.getAllToDos();
+		
+		model.addAttribute("todos", todos);
+		
+		return "todoView";
+	}
+
+}
---
+++ src/main/java/com/example/springkadaitodo/repository/ToDoRepository.java
@@ -0,0 +1,12 @@
+package com.example.springkadaitodo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.springkadaitodo.entity.ToDo;
+
+
+@Repository
+public interface ToDoRepository extends JpaRepository <ToDo, Integer> {
+
+}
